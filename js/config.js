17d52c4f79a89ff0345aeb7490c337dfb04b6787
thIAguinho <iathiaguinho@gmail.com>
2025-09-16 14:34:34 -0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,20 @@
+// ===================================================================
+// ARQUIVO DE CONFIGURAÇÃO - NÃO COMPARTILHE ESTE ARQUIVO PUBLICAMENTE
+// ===================================================================
+
+// Configuração do Firebase
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBppgEyO9visa_Bstktf_WFW80w-SOWG3M",
+  authDomain: "corrida-rp.firebaseapp.com",
+  databaseURL: "https://corrida-rp-default-rtdb.firebaseio.com",
+  projectId: "corrida-rp",
+  storageBucket: "corrida-rp.firebasestorage.app",
+  messagingSenderId: "268310025993",
+  appId: "1:268310025993:web:1134b24d09b54d3f8aa5d8"
+};
+
+// Configuração do Cloudinary
+const CLOUDINARY_CONFIG = {
+  CLOUD_NAME: "dckxhoyug",
+  UPLOAD_PRESET: "corrida" // Pelo que você enviou, o nome do seu preset é "corrida"
+};
